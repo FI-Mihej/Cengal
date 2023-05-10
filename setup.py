fb0a784b370beff3dbb4eaca7d8c69967f833b81
ButenkoMS <gtalk@butenkoms.space>
2023-05-11 01:44:08 +0300

    Version up to v3.1.17

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ __author__ = "ButenkoMS <gtalk@butenkoms.space>"
 __copyright__ = "Copyright © 2012-2023 ButenkoMS. All rights reserved. Contacts: <gtalk@butenkoms.space>"
 __credits__ = ["ButenkoMS <gtalk@butenkoms.space>", ]
 __license__ = "Apache License, Version 2.0"
-__version__ = "3.1.16"
+__version__ = "3.1.17"
 __maintainer__ = "ButenkoMS <gtalk@butenkoms.space>"
 __email__ = "gtalk@butenkoms.space"
 # __status__ = "Prototype"
@@ -96,14 +96,14 @@ prepare_cflags()
 setuptools.setup(
     name='cengal',
     version=__version__,
-    author=__author__,
+    author='ButenkoMS',
     author_email=__email__,
     description='General purpose library',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/FI-Mihej/Cengal',
     license=__license__,
-    keywords='general purpose library async loop tools utilities bytecode introspection parsing',
+    keywords='async loop, coroutine, async Qt, async Tkinter, bytecode manipulation, introspection, text parsing',
     py_modules=['cengal'],
     # package_dir={'': path_relative_to_src('')},
     packages=find_good_packages(),
